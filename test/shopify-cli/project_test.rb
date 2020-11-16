1056c1d5e4326171e012daf77c0b96d6e2efb38a
Andy Waite <andyw8@users.noreply.github.com>
2020-11-16 09:34:17 -0500

    Remove dupe test

--- test/shopify-cli/project_test.rb
+++ test/shopify-cli/project_test.rb
@@ -39,16 +39,6 @@ module ShopifyCli
     end
 
     def test_write_writes_yaml_with_shopify_organization_field
-      Shopifolk.stubs(:acting_as_shopify_organization?).returns(true)
-      Dir.stubs(:pwd).returns(@context.root)
-      FileUtils.touch(".shopify-cli.yml")
-      ShopifyCli::Project.write(@context, project_type: :node, organization_id: 42)
-      assert_equal :node, Project.current.config['project_type']
-      assert_equal 42, Project.current.config['organization_id']
-      assert Project.current.config['shopify_organization']
-    end
-
-    def test_write_writes_yaml_with_shopify_organization_field
       create_empty_config
       Shopifolk.stubs(:acting_as_shopify_organization?).returns(true)
       ShopifyCli::Project.write(@context, project_type: :node, organization_id: 42)
